0ddfaf844325f22bc74ad268e318a4e5a8316b93
Suhel Khan <suhelkhan87514@gmail.com>
2023-05-27 13:30:08 +0530

    Added more pattern

--- example8.glsl
+++ example8.glsl
@@ -1,8 +1,8 @@
 vec3 palette(float t) {
     vec3 a = vec3(0.5, 0.5, 0.5);
     vec3 b = vec3(0.5, 0.5, 0.5);
-    vec3 c = vec3(0.1, 0.1, 0.1);
-    vec3 d = vec3(0.808, 0.820, 0.161);
+    vec3 c = vec3(0.380, 0.290, 0.039);
+    vec3 d = vec3(0.220, 0.067, 0.373);
 
     return a + b * cos(6.28318 * (c * t + d));
 
@@ -14,18 +14,18 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 uv0 = uv;
     vec3 finalColor = vec3(0.0);
 
-    for(float i = 0.0; i < 10.0; i++) {
+    for(float i = 0.0; i < 5.0; i++) {
 
-        uv = fract(uv * 2.0) - 0.5;
+        uv = fract(uv * 3.0) - 0.5;
 
         float d = length(uv);
 
-        vec3 col = palette(length(uv0) + iTime * .4);
+        vec3 col = palette(length(uv) + iTime * .6);
 
-        d = sin(d * 12. + iTime) / 6.;
+        d = sin(d * 8. + iTime * .4) / 2.;
         d = abs(d);
 
-        d = 0.04 / d;
+        d = 0.02 / d;
 
         finalColor += col * d;
     }
--- example9.glsl
+++ example9.glsl
@@ -1,8 +1,8 @@
 vec3 palette(float t) {
     vec3 a = vec3(0.5, 0.5, 0.5);
-    vec3 b = vec3(0.5, 0.5, 0.5);
-    vec3 c = vec3(0.1, 0.1, 0.1);
-    vec3 d = vec3(0.808, 0.820, 0.161);
+    vec3 b = vec3(0.800, 0.565, 0.996);
+    vec3 c = vec3(0.043, 0.278, 0.357);
+    vec3 d = vec3(0.173, 0.427, 0.365);
 
     return a + b * cos(6.28318 * (c * t + d));
 
@@ -14,18 +14,18 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 uv0 = uv;
     vec3 finalColor = vec3(0.0);
 
-    for(float i = 0.0; i < 10.0; i++) {
+    for(float i = 0.0; i < 4.0; i++) {
 
-        uv = fract(uv * 2.0) - 0.5;
+        uv = fract(uv * 1.5) - 0.5;
 
-        float d = length(uv);
+        float d = length(uv) * exp(-length(uv0));
 
-        vec3 col = palette(length(uv0) + iTime * .4);
+        vec3 col = palette(length(uv0) + i * 4. + iTime * .4);
 
-        d = sin(d * 12. + iTime) / 6.;
+        d = sin(d * 28. + iTime) / 8.;
         d = abs(d);
 
-        d = 0.04 / d;
+        d = pow(0.006 / d, 1.5);
 
         finalColor += col * d;
     }
